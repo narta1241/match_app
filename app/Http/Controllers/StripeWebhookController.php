0f4b08a2317b212bdf5ddd0907cd9d2ed78efa57
narta1241 <end.indigo@gmail.com>
2021-06-09 14:17:25 +0000

    20210609

---
+++ app/Http/Controllers/StripeWebhookController.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
+
+class StripeWebhookController extends CashierController
+{
+    public function handleCheckoutSessionCompleted(array $payload)
+    {
+        $session = $payload['data']['object'];
+        $user = User::findOrFail($session['client_reference_id']);
+
+        DB::transaction(function () use ($session, $user) {
+            $user->update(['stripe_id' => $session['customer']]);
+
+            $user->subscriptions()->create([
+                'name'          => 'default',
+                'stripe_id'     => $session['subscription'],
+                'stripe_status' => 'active' // Or use "active" if you don't provide a trial
+            ]);
+        });
+
+        return $this->successMethod();
+    }
+}
